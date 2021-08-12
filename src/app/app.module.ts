081c373bef7f91349dab724d680fce24d8f4cd33
amabelle <amabelletr@gmail.com>
2021-08-12 16:31:20 +0300

    outputing posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,20 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +31,7 @@ import { HeaderComponent } from './header/header.component';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  panelOpenState = false;
+
+  posts = [
+    {title: 'first post', content: 'this is the post\'s content...'},
+    {title: 'second post', content: 'this is the sec post\'s content...'},
+    {title: 'third post', content: 'this is the 3 post\'s content...'},
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
